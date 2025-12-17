fd63f6bb7eef233f361aebff7f0edc5e0d703b9f
MMahmud24 <mahinmahmud2406@gmail.com>
2025-12-17 11:54:08 -0500

    login and signup files added

--- app.py
+++ app.py
@@ -10,5 +10,14 @@ def index():
 def create():
     return render_template("create.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
